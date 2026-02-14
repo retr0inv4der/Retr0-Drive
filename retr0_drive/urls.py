8180fa795ac9c26ff7d4b7ea4d16edc86c6c410c
retr0inv4der <retr0.inv4der@gmail.com>
2026-02-14 17:44:46 +0330

    user register api

--- retr0_drive/urls.py
+++ retr0_drive/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from users import urls as UserURLs
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("user/" , UserURLs)
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,20 @@
 
-from rest_framework.decorators import api_view , permission_classes
+from rest_framework.views import APIView
 from rest_framework.response import Response 
 from rest_framework import status
+from rest_framework import request
+#sers
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class UserRegisterAPI(APIView) : 
+
+    def post(self, request : request):
+        ser = UserSerializer(request.data)
+        ser.is_valid(raise_exception= True)
+        ser.save()
+        return Response({
+            "message" : "user registered successfully"} ,
+             status= status.HTTP_201_CREATED
+            )
